c9f71a8c58bf6ae36f97a803fd7fbe96c63f8b1d
S556508 <112007811+S556508@users.noreply.github.com>
2022-10-21 21:22:33 -0500

    made the changes in mydata file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var indexRouter = require('./routes/mydata');
+var mydataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -16,13 +16,13 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/mydata', mydataRouter);
-
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
