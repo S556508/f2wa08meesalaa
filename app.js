8bcc3eafb5e4616a27f1a15c2c0d908922c37c2e
S556508 <112007811+S556508@users.noreply.github.com>
2022-10-21 21:08:35 -0500

    made the changes in mydatafile

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/mydata');
+app.use('/mydata', mydataRouter);
 
 app.use('/users', usersRouter);
 
