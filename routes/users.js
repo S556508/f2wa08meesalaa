3b48e10fd1c26bd4f33d7b9b478a34cd41749870
S556508 <112007811+S556508@users.noreply.github.com>
2022-10-21 21:44:05 -0500

    made the changes in users.js file

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-let time=0;
+let time = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  time = time+1;
-  res.send('User accesses are: ${time}');
+  time=time+1;
+  res.send('User accesses are:' +(time));
 });
 
 module.exports = router;
